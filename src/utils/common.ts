5facb5837c25d1c32dd7f8117d89d44704c3e6a6
carolCross <1498354528@qq.com>
2024-11-11 17:10:09 +0800

    fix: change import

---
+++ src/utils/common.ts
@@ -0,0 +1,28 @@
+import { CommonAPIs } from "@/utils";
+import { SourceLocation } from '@babel/types';
+
+/** 是否支持当前Api */
+export const isSupportApi = (fieldType: string): boolean => {
+  if (!fieldType) {
+    return false;
+  }
+  const firstApi = fieldType.split(".")?.[0] || "unknow";
+  return CommonAPIs.includes(firstApi);
+};
+
+/** loc 生成code poi */
+export const locToCodePoi = (loc: SourceLocation): CodePoi | null => {
+  if (!loc) return null
+  const start = loc.start;
+  const end = loc.end;
+  return {
+    start: {
+      x: start.line,
+      y: start.column,
+  },
+  end: {
+      x: end.line,
+      y: end.column,
+  }
+  }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,32 +1,8 @@
-import { CommonAPIs } from "./constant";
-import { SourceLocation } from '@babel/types';
-
+/** constant config */
 export * from './constant';
+/** tsTypeToAstNode */
 export * from './tsTypeToAstNode';
+/** astNodeToJsType */
 export * from './astNodeToJsType';
-
-/** 是否支持当前Api */
-export const isSupportApi = (fieldType: string): boolean => {
-  if (!fieldType) {
-    return false;
-  }
-  const firstApi = fieldType.split(".")?.[0] || "unknow";
-  return CommonAPIs.includes(firstApi);
-};
-
-/** loc 生成code poi */
-export const locToCodePoi = (loc: SourceLocation): CodePoi | null => {
-  if (!loc) return null
-  const start = loc.start;
-  const end = loc.end;
-  return {
-    start: {
-      x: start.line,
-      y: start.column,
-  },
-  end: {
-      x: end.line,
-      y: end.column,
-  }
-  }
-}
+/** common fuc */
+export * from './common';
