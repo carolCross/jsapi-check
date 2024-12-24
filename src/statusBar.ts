aa1507d06ac30f271ca25ed9853ec4fab469fbc8
carolCross <1498354528@qq.com>
2024-12-24 19:47:12 +0800

    feat: 支持模式选择-alipayHK/wechat 开发模式

--- src/statusBar.ts
+++ src/statusBar.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { chromeVersion, setChromeVersion } from './versionControl';
-import { BarText, BarTooltip, BarCommand } from "./utils/constant";
+import { InputStatusBarText, InputStatusTooltip, InputStatusCommand, ModeStatusBarText, ModeStatusCommand, ModeStatusTooltip, ModeChromeVersionMap } from "./utils/constant";
 
 /** props */
 type PropsType = {
@@ -12,7 +12,13 @@ type PropsType = {
 /* 底部状态栏 */
 export default class StatusBar {
   props = {} as PropsType;
-  statusBarItem = {} as vscode.StatusBarItem;
+  // 状态栏 输入修改版本
+  inputStatusBar = {} as vscode.StatusBarItem;
+  // 模式版本 选择模式
+  modeStatusBar = {} as vscode.StatusBarItem;
+  // 当前模式
+  currentMode = 'alipayhk' as keyof typeof ModeChromeVersionMap;
+
   constructor(pr: PropsType) {
     this.props = pr;
     this.initStatusBar();
@@ -20,41 +26,74 @@ export default class StatusBar {
   /** 加载状态栏 */
   initStatusBar = () => {
     // 创建状态栏项
-    this.statusBarItem = vscode.window.createStatusBarItem(
+    this.inputStatusBar = vscode.window.createStatusBarItem(
       vscode.StatusBarAlignment.Right,
       100
     );
-    this.statusBarItem.text = `${BarText}${chromeVersion}`;
-    this.statusBarItem.tooltip = BarTooltip;
-    this.statusBarItem.command = BarCommand;
+   
+    this.inputStatusBar.text = `${InputStatusBarText}${chromeVersion}`;
+    this.inputStatusBar.tooltip = InputStatusTooltip;
+    this.inputStatusBar.command = InputStatusCommand;
     // 显示状态栏项
-    this.statusBarItem.show();
-    this.props.context.subscriptions.push(this.statusBarItem);
+    this.inputStatusBar.show();
+    this.props.context.subscriptions.push(this.inputStatusBar);
     this.initChromeVersion();
+    // 模式版本 选择模式
+    this.modeStatusBar = vscode.window.createStatusBarItem(
+      vscode.StatusBarAlignment.Right,
+      100
+    );
+    this.modeStatusBar.text = `${ModeStatusBarText}${this.currentMode}`;
+    this.modeStatusBar.tooltip = ModeStatusTooltip;
+    this.modeStatusBar.command = ModeStatusCommand;
+    // 显示状态栏项
+    this.modeStatusBar.show();
+    this.props.context.subscriptions.push(this.modeStatusBar);
+    this.initModeVersion();
   };
   /**  注册ChromeVerison 版本 */
   initChromeVersion = () => {
     const chromeVersionCommand = vscode.commands.registerCommand(
-      BarCommand,
+      InputStatusCommand,
       async () => {
         const input = await vscode.window.showInputBox({
-          prompt: "Enter the target Chrome version",
+          prompt: "请输入chrome版本号",
           value: chromeVersion.toString(),
           validateInput: (value) =>
-            isNaN(Number(value)) ? "Please enter a valid number" : null,
+            isNaN(Number(value)) ? "请输入正确的值" : null,
         });
 
         if (input) {
           setChromeVersion(parseInt(input, 10));
-          this.statusBarItem.text = `Chrome Version: ${chromeVersion}`;
+          this.inputStatusBar.text = `当前版本: ${chromeVersion}`;
           vscode.window.showInformationMessage(
-            `Chrome version set to ${chromeVersion}`
+            `chrome 设置为 ${chromeVersion}`
           );
           this.setUpdateDiagnostics();
         }
       }
     );
     this.props.context.subscriptions.push(chromeVersionCommand);
+  };
+  /**  注册ModeVerison 版本 */
+  initModeVersion = () => {
+    const chromeModeCommand = vscode.commands.registerCommand(
+      ModeStatusCommand,
+      async () => {
+        const selectedMode = await vscode.window.showQuickPick(Object.keys(ModeChromeVersionMap), {
+          placeHolder: '请选择一个开发模式'
+        });
+        if (selectedMode) {
+          setChromeVersion(ModeChromeVersionMap[this.currentMode]);
+          this.currentMode = selectedMode as keyof typeof ModeChromeVersionMap;
+          this.modeStatusBar.text = `${ModeStatusBarText}${this.currentMode}`;
+          console.log('selectedMode=====', selectedMode, ModeChromeVersionMap[this.currentMode])
+          this.inputStatusBar.text = `Chrome Version: ${ModeChromeVersionMap[this.currentMode]}`;
+          vscode.window.showInformationMessage(`已切换到 ${selectedMode} 模式`);
+        }
+      }
+    );
+    this.props.context.subscriptions.push(chromeModeCommand);
   };
 
   /** 更新所有打开文档的诊断 */
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,19 +1,32 @@
 /** 常量列表 */
 
 /** 默认版本 */
-export const DefaultVerison = 0; 
+export const DefaultVerison = 76; 
 /** 目前支持检测语言 */
 export const supportLanguageList = [
     'javascript', 'typescript', 'typescriptreact', 'vue'
 ];
 /** 默认报错提示指令 */
 export const DiagnosticCommand = "chrome-compatibility";
-/** 底部状态栏标题 */
-export const BarText = 'Chrome Version: ';
-/** 底部状态栏tooltip */
-export const BarTooltip = 'Click to change Chrome version';
-/** 底部状态栏command */
-export const BarCommand = 'jsapi_check.changeChromeVersion';
 /** 当前支持检测类型 */
 export const CommonAPIs = [ 'Array', 'Object', 'Function', 'String', 'Number', 'Boolean', 'Promise', 'Map', 'Set',
     'Date', 'RegExp', 'Math', 'JSON', 'fetch', 'console', 'Error', 'Symbol', 'Intl']
+    /** 底部状态栏标题 */
+export const InputStatusBarText = 'Chrome 版本: ';
+/** 底部状态栏tooltip */
+export const InputStatusTooltip = '更换chrome版本';
+/** 底部状态栏command */
+export const InputStatusCommand = 'jsapi_check.changeChromeVersion';
+
+/** 底部状态栏标题 */
+export const ModeStatusBarText = '当前模式: ';
+/** 底部状态栏tooltip */
+export const ModeStatusTooltip = '请选择开发者模式';
+/** 底部状态栏command */
+export const ModeStatusCommand = 'jsapi_check.changeMode';
+
+/** 版本控制 */
+export const ModeChromeVersionMap = {
+    alipayhk: DefaultVerison,
+    wechat: 107
+}
